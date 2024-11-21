60a148ccce957c5d844ab3a5dcaf5972c6c816c6
Sonora Mata <smata7@gatech.edu>
2024-11-21 14:48:24 -0600

    reconfigure homepage into different entry tabs

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -8,12 +8,12 @@
 
 //flutter libraries
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 //project files
 import 'package:manga_matrix/listscreen.dart';
 import 'package:manga_matrix/profilescreen.dart';
 import 'package:manga_matrix/searchscreen.dart';
+import 'package:manga_matrix/hometab.dart';
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -23,7 +23,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   final List<Widget> _tabs = [
-    HomeScreen(),
+    NestedTabBar('Entry'),
     ListScreen(),
     SearchScreen(),
     ProfileScreen(),
@@ -42,7 +42,7 @@ class _MyHomePageState extends State<MyHomePage> {
             labelColor: Colors.pink[100],
             indicatorColor: Colors.pink[100],
             tabs: [
-              Tab(icon: Icon(Icons.home), text: 'Home'),
+              Tab(icon: Icon(Icons.format_list_bulleted_add), text: 'Entry'),
               Tab(icon: Icon(Icons.list_alt), text: 'My List'),
               Tab(icon: Icon(Icons.search), text: 'Search'),
               Tab(icon: Icon(Icons.person), text: 'Profile'),
@@ -57,329 +57,52 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-String typesdropdownvalue = 'Manga';
-var types = [
-  'Manga',
-  'Manhwa',
-  'Webtoon',
-  'Light novel',
-  'Other',
-];
+class NestedTabBar extends StatefulWidget {
+  const NestedTabBar(this.outerTab, {super.key});
 
-String statusesdropdownvalue = 'Ongoing';
-var statuses = [
-  'Ongoing',
-  'Completed',
-  'Hiatus'
-];
+  final String outerTab;
 
-String userstatusesdropdownvalue = 'Caught up';
-var userStatuses = [
-  'Caught up',
-  'Still Reading',
-  'Dropped',
-  'Want to read'
-];
-
-class HomeScreen extends StatefulWidget {
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<NestedTabBar> createState() => _NestedTabBarState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _NestedTabBarState extends State<NestedTabBar>
+    with TickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: DecoratedBox(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("images/narumi_reading_stand.jpg"), fit: BoxFit.cover),
+    return Column(
+      children: <Widget>[
+        TabBar.secondary(
+          controller: _tabController,
+          tabs: const <Widget>[
+            Tab(text: 'Manual Entry'),
+            Tab(text: 'List Entry'),
+          ],
         ),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Container(
-                      color: Colors.white,
-                      child: Text('Multi entry:', 
-                        textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 20, 
-                            )
-                          ),
-                    ),
-                  )
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                  new Flexible(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.pink[50],
-                          border: OutlineInputBorder(),
-                          labelText: "List entry",
-                          hintText: "Enter multiple manga names with chapter numbers in list form"
-                        ),
-                        keyboardType: TextInputType.multiline,
-                        maxLines: null,
-                      )
-                    ),
-                  ]
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Container(
-                      color: Colors.white,
-                      child: Text('Manual entry:', 
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 20, 
-                              color: Colors.black,
-                            )
-                          ),
-                    ),
-                  )
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      color: Colors.white,
-                      child: Text('* = Required', style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),)
-                      )
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(children: [
-                  new Flexible(
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.pink[50],
-                        border: OutlineInputBorder(),
-                        labelText: "*Manga Name"
-                      ),
-                    )
-                  )
-                ],),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(children: [
-                  new Flexible(
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.pink[50],
-                        border: OutlineInputBorder(),
-                        labelText: "Publisher"
-                      ),
-                    )
-                  )
-                ],),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                  SizedBox(
-                      width: 100,
-                      height: 50,
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.pink[50],
-                          border: OutlineInputBorder(),
-                          labelText: "*Chapters"
-                        ),
-                        inputFormatters: <TextInputFormatter>[
-                        FilteringTextInputFormatter.digitsOnly
-                      ],
-                      )
-                    ),
-                  Container(
-                    color: Colors.white,
-                    child: Text(' out of ', style: TextStyle(fontSize: 14))
-                    ),
-                  SizedBox(
-                      width: 100,
-                      height: 50,
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.pink[50],
-                          border: OutlineInputBorder(),
-                          labelText: "*Total"
-                        ),
-                        inputFormatters: <TextInputFormatter>[
-                          FilteringTextInputFormatter.digitsOnly
-                        ],
-                      )
-                    ),
-                ],),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: 250,
-                  color: Colors.white,
-                  child: Row(
-                    children: [
-                      Text('*Manga status: '),
-                      SizedBox(width: 5,),
-                      DropdownButton(
-                        dropdownColor: Colors.pink[50],
-                        value: statusesdropdownvalue,
-                        items: statuses.map((String statuses){
-                          return DropdownMenuItem(
-                            value: statuses,
-                            child: Text(statuses),
-                            );
-                        }).toList(), 
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            statusesdropdownvalue = newValue!;
-                          });
-                        }
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  color: Colors.white,
-                  width: 250,
-                  child: Row(
-                    children: [
-                      Text('*User status: '),
-                      SizedBox(width: 5,),
-                      DropdownButton(
-                        value: userstatusesdropdownvalue,
-                        items: userStatuses.map((String userStatuses){
-                          return DropdownMenuItem(
-                            value: userStatuses,
-                            child: Text(userStatuses),
-                            );
-                        }).toList(), 
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            userstatusesdropdownvalue = newValue!;
-                          });
-                        }
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  color: Colors.white,
-                  child: Row(children: [
-                    Text('*Type:    '),
-                    DropdownButton(
-                      value: typesdropdownvalue,
-                      items: types.map((String types){
-                        return DropdownMenuItem(
-                          value: types,
-                          child: Text(types),
-                          );
-                      }).toList(), 
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          typesdropdownvalue = newValue!;
-                        });
-                      }),
-                      SizedBox(width: 6,),
-                      SizedBox(
-                        width: 80,
-                        height: 60,
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Colors.pink[50],
-                            border: OutlineInputBorder(),
-                            labelText: "*Rating"
-                          ),
-                          validator: (value) {
-                            if (value!.isEmpty) { //if value is null (! is null check)
-                              return 'Please enter the Overall Rating';
-                            } else if (int.parse(value) < 1 || int.parse(value) > 10) {
-                              return 'The rating must be between 1 and 10';
-                            }
-                            return null;
-                          },
-                          inputFormatters: <TextInputFormatter>[
-                            FilteringTextInputFormatter.digitsOnly
-                          ],
-                          maxLength: 2,
-                          maxLengthEnforcement: MaxLengthEnforcement.enforced,
-                        )
-                      ),
-                      Text(' out of 10')
-                  ],),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Row(
-                  children: [
-                    new Flexible(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.pink[50],
-                          border: OutlineInputBorder(),
-                          labelText: "Comments"
-                        ),
-                        keyboardType: TextInputType.multiline,
-                        maxLines: null,
-                      )
-                    ),
-                  ],
-                ),
-              ),
-              Row(children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: WidgetStatePropertyAll(Colors.grey[300]), 
-                      foregroundColor: WidgetStatePropertyAll(Colors.pink[100])
-                    ),
-                    child: Text('Submit'),
-                    onPressed: (){}
-                    ),
-                )
-              ],)
+        Expanded(
+          child: TabBarView(
+            controller: _tabController,
+            children: <Widget>[
+              ManualEntryTab(),
+              ListEntryTab()
             ],
           ),
         ),
-      )
+      ],
     );
   }
 }
