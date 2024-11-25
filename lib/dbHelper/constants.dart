f87cf5b0ed64f1db1bae2fa7526633877ebd25dd
Sonora Mata <smata7@gatech.edu>
2024-11-24 21:44:38 -0600

    fix gitignore

--- lib/dbHelper/constants.dart
+++
@@ -1,9 +0,0 @@
-/* constants.dart
- * does: sets constants for db vars
- */
-
-const MONGO_URL = "";
-const USER_COLLECTION = "";
-const MANGA_COLLECTION = "";
-const ENTRY_COLLECTION = "";
-const TAGS_COLLECTION = "";
